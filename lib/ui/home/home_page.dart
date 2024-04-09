218cd2f2f683457f2e213066ec167327f400653c
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-09 15:10:00 +0900

    FEAT: home

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:counter/ui/_constant/theme/devcoop_text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:counter/ui/components/navbar.dart';
 
@@ -13,7 +14,9 @@ class MyHome extends StatelessWidget {
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [],
+            children: [
+              Text("더 많은 기능을 준비중입니다", style: DevCoopTextStyle.bold_50)
+            ],
           ),
         ],
       ),
