ce4edad23865a978a7c3df95f5fec880676153b2
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-09 15:08:42 +0900

    FEAT: 메뉴바

--- lib/ui/components/navbar.dart
+++ lib/ui/components/navbar.dart
@@ -1,24 +1,96 @@
 import 'package:counter/ui/_constant/theme/devcoop_text_style.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-PreferredSizeWidget? navBar() {
+import 'package:counter/ui/_constant/theme/devcoop_colors.dart';
+
+Widget _buildCustomButton({
+  required String text,
+  required IconData icon,
+  required VoidCallback onPressed,
+}) {
+  return ElevatedButton.icon(
+    onPressed: onPressed,
+    icon: Icon(icon, size: 100),
+    label: Text(text, style: DevCoopTextStyle.bold_30),
+    style: ElevatedButton.styleFrom(
+      foregroundColor: Colors.white,
+      backgroundColor: DevCoopColors.primary, // 버튼 텍스트 색상
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      elevation: 5.0,
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+      textStyle: DevCoopTextStyle.bold_30.copyWith(color: Colors.white),
+    ),
+  );
+}
+
+List<Widget> navBarData = [
+  _buildCustomButton(
+    text: "결제하러 가기",
+    icon: Icons.payment,
+    onPressed: () {
+      Get.offAndToNamed("/barcode");
+    },
+  ),
+  const SizedBox(height: 20),
+  _buildCustomButton(
+    text: "비밀번호 변경",
+    icon: Icons.lock_open,
+    onPressed: () {
+      Get.offAndToNamed("/pin/change");
+    },
+  ),
+  const SizedBox(height: 20),
+  _buildCustomButton(
+    text: "문의하러 가기",
+    icon: Icons.question_answer,
+    onPressed: () {},
+  ),
+];
+
+void _showPopupMenu(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext bc) {
+      return ListView.builder(
+        itemCount: 10, // 리스트 아이템의 개수
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: navBarData[index],
+            onTap: () => Navigator.pop(context), // 아이템 선택 시 모달 닫기
+          );
+        },
+      );
+    },
+  );
+}
+
+PreferredSizeWidget? navBar(BuildContext context) {
   return PreferredSize(
-    preferredSize: Size.fromHeight(100),
+    preferredSize: const Size.fromHeight(50),
     child: AppBar(
       automaticallyImplyLeading: false,
       backgroundColor: Colors.white,
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
+          const Text(
             "BSM 셀프계산대",
-            style: DevCoopTextStyle.bold_30,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+            ),
           ),
           IconButton(
-            onPressed: () {},
-            icon: Icon(
+            onPressed: () {
+              _showPopupMenu(context);
+            },
+            icon: const Icon(
               Icons.menu,
-              size: 40,
+              size: 50,
             ),
           )
         ],
--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:counter/ui/_constant/theme/devcoop_colors.dart';
-import 'package:counter/ui/_constant/theme/devcoop_text_style.dart';
 import 'package:counter/ui/components/navbar.dart';
 
 class MyHome extends StatelessWidget {
@@ -10,62 +7,15 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: navBar(),
-      body: Column(
+      appBar: navBar(context),
+      body: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _buildCustomButton(
-                text: "결제하러 가기",
-                icon: Icons.payment,
-                onPressed: () {
-                  Get.offAndToNamed("/barcode");
-                },
-              ),
-              const SizedBox(height: 20),
-              _buildCustomButton(
-                text: "비밀번호 변경",
-                icon: Icons.lock_open,
-                onPressed: () {
-                  Get.offAndToNamed("/pin/change");
-                },
-              ),
-              const SizedBox(height: 20),
-              _buildCustomButton(
-                text: "문의하러 가기",
-                icon: Icons.question_answer,
-                onPressed: () {},
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 90,
+            children: [],
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildCustomButton({
-    required String text,
-    required IconData icon,
-    required VoidCallback onPressed,
-  }) {
-    return ElevatedButton.icon(
-      onPressed: onPressed,
-      icon: Icon(icon, size: 100),
-      label: Text(text, style: DevCoopTextStyle.bold_30),
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.white,
-        backgroundColor: DevCoopColors.primary, // 버튼 텍스트 색상
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-        elevation: 5.0,
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-        textStyle: DevCoopTextStyle.bold_30.copyWith(color: Colors.white),
       ),
     );
   }
