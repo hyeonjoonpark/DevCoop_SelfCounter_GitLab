24022110b73fbbf0eec3912c0bac8fa2631b998e
KyuBong Kim <hwansi@bssm.hs.kr>
2024-05-17 06:39:19 +0000

    Update file ci_script.ps1

--- ci_script.ps1
+++ ci_script.ps1
@@ -8,7 +8,6 @@ switch ($stage) {
     "setup" {
         echo "Setting up Flutter environment..."
         flutter --version
-	git pull
     }
     "build" {
         echo "Running pub get..."
@@ -18,7 +17,24 @@ switch ($stage) {
     }
     "deploy" {
         echo "Deploying the application..."
-        build\windows\x64\runner\Release\counter.exe
+        # 기존 프로세스 종료
+        $process = Get-Process -Name "counter" -ErrorAction SilentlyContinue
+        if ($process) {
+            echo "Stopping existing counter.exe process..."
+            Stop-Process -Name "counter" -Force
+        } else {
+            echo "No existing counter.exe process found."
+        }
+        
+        # 새로운 프로세스 시작
+        $exePath = "C:\Users\KB\Devcoop\build\windows\x64\runner\Release\counter.exe"
+        if (Test-Path $exePath) {
+            echo "Starting new counter.exe process..."
+            Start-Process -FilePath $exePath
+        } else {
+            echo "counter.exe not found at $exePath"
+            exit 1
+        }
     }
     default {
         echo "Invalid stage specified"
