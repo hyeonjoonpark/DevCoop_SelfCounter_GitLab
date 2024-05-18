77f821208500531b7d8ef41e8ed184bd419c41fc
Hwansi <59302284+KimKyuBong@users.noreply.github.com>
2024-05-18 14:27:14 +0900

    UPDATE

--- lib/secure/db.dart
+++ lib/secure/db.dart
@@ -7,9 +7,12 @@ class DbSecure {
 
   static Future<DbSecure> load() async {
     // 환경 변수 읽기, 없으면 기본 값으로 설정
-    String dbHost = Platform.environment['DB_HOST'] ?? 'localhost:8080';
+    String dbHost = Platform.environment['DB_HOST'] ?? 'localhost';
     return DbSecure._(dbHost);
   }
+
+  // 기본 생성자: 기본 값으로 초기화
+  DbSecure() : dbHost = Platform.environment['DB_HOST'] ?? 'localhost';
 
   String get DB_HOST => dbHost;
 }
