316db72e0ebdecbb6ce976288acb8915abe5dfd5
KyuBong Kim <hwansi@bssm.hs.kr>
2024-05-20 23:26:51 +0000

    Update 2 files
    - /ci_script.ps1
    - /.gitlab-ci.yml

--- ci_script.ps1
+++ ci_script.ps1
@@ -1,9 +1,10 @@
 param (
     [string]$stage
 )
+
 cd C:\Users\KB\Devcoop\devcoop_self_counter_v1
 
-# 환경 변수 읽기
+# 환경 변수 설정
 $env:DB_HOST = $env:DB_HOST
 echo "DB_HOST: $env:DB_HOST"
 
@@ -26,6 +27,9 @@ switch ($stage) {
         flutter pub get
         echo "Building the Flutter application..."
         flutter build windows --release --dart-define=DB_HOST=$env:DB_HOST
+
+        # 빌드 완료 후 메시지 출력
+        echo "Flutter application built successfully."
     }
     "deploy" {
         echo "Deploying the application..."
@@ -42,6 +46,17 @@ switch ($stage) {
         # 수동으로 생성한 작업 스케줄러 작업 실행
         $taskName = "StartCounterExeTask"
         schtasks /run /tn $taskName
+
+        # 배포 완료 후 메시지 출력
+        echo "Flutter application deployed successfully."
+        
+        # 로그 파일 읽기 및 출력
+        $logFilePath = "C:\Users\KB\Devcoop\devcoop_self_counter_v1\db_host_log.txt"
+        if (Test-Path $logFilePath) {
+            Get-Content $logFilePath
+        } else {
+            echo "Log file not found."
+        }
     }
     default {
         echo "Invalid stage specified"
