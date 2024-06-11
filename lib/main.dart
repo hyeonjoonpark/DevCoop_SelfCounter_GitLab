51ae1d3ef0de19d52be8e63ed945a33dc242e6f4
hyeonjoonpark <pjjoon1379@gmail.com>
2024-06-11 12:28:40 +0900

    FIX: folder name

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: '부산소프트웨어마이스터고등학교 아리소리 셀프계산대',
+      title: '부산소프트웨어마이스터고등학교 아리소리 셀프계산대.',
       debugShowCheckedModeBanner: false,
       getPages: appRouter,
       initialRoute: '/',
