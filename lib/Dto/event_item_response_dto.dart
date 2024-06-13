a55b495c59df89443f597f488c891b11bebca22d
hyeonjoonpark <pjjoon1379@gmail.com>
2024-06-13 11:16:16 +0900

    REMOVE: 필요없는 EventItemResponseDto 요소 삭제

--- lib/Dto/event_item_response_dto.dart
+++ lib/Dto/event_item_response_dto.dart
@@ -3,16 +3,12 @@ class EventItemResponseDto {
   final String itemName;
   final int itemPrice;
   final String event;
-  final String? eventStartDate;
-  final String? eventEndDate;
 
   EventItemResponseDto({
     required this.barcode,
     required this.itemName,
     required this.itemPrice,
     required this.event,
-    this.eventStartDate,
-    this.eventEndDate,
   });
 
   factory EventItemResponseDto.fromJson(Map<String, dynamic> json) {
@@ -21,8 +17,6 @@ class EventItemResponseDto {
       itemName: json['itemName'],
       itemPrice: json['itemPrice'],
       event: json['event'],
-      eventStartDate: json['eventStartDate'],
-      eventEndDate: json['eventEndDate'],
     );
   }
 }
